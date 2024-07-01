66fee1087e7ca885a2a3d34981a22a705fcdd01e
diothedog <88989273+diothedog@users.noreply.github.com>
2024-07-01 16:13:28 -0500

    Create table in html and create js function to add each book to table

--- script.js
+++ script.js
@@ -13,5 +13,23 @@ function addBookToLibrary(title, author, pages, read) {
 }
 
 function displayBooks(library) {
-    library.forEach((book) => console.log(book));
+    // Select table body
+    let body = document.querySelector("tbody");
+
+    // Clear table body
+    while(body.firstElementChild) {
+        body.firstElementChild.remove();
+    }
+
+    // Add each book to table body
+    library.forEach((book) => {
+        let row = document.createElement("tr");
+        row.innerHTML = `<td>${book.title}</td>
+                        <td>${book.author}</td>
+                        <td>${book.pages}</td>
+                        <td>${book.read}</td`;
+        row.classList.add("book-row");
+        body.appendChild(row);
+    });
 }
+
